8f6c20f47c5732fd301f37c46d420cba418f55d1
Joacohbc <Joacohbc@test.test>
2025-05-09 11:51:02 -0300

    Generate a random password for devuser and update initial password file permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,12 +11,15 @@ if [ $(id -u devuser 2>/dev/null || echo -1) -ge 0 ]; then
 else
     # Crea el usuario devuser con la contraseña devuser y le da permisos de sudo
     useradd -m -s /bin/bash devuser
-    echo 'devuser:devpass' | chpasswd
+    DEV_PASSWORD=$(pwgen -s 32) # Generate a 16-character random password
+    echo "devuser:$DEV_PASSWORD" | chpasswd
     usermod -aG sudo devuser
     
     # To disable Docker-outside-Docker, comment out the next line
     usermod -aG docker devuser # Add devuser to docker group
-    echo "devpass" > /home/devuser/initial_password.txt
+    echo "$DEV_PASSWORD" > /home/devuser/initial_password.txt
+    chown devuser:devuser /home/devuser/initial_password.txt # Ensure devuser owns the file
+    chmod 600 /home/devuser/initial_password.txt # Set appropriate permissions
     echo "devuser password: $(cat /home/devuser/initial_password.txt)"
 fi
 
